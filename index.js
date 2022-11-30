0b43ec189e29b83519596f353641e5d9cc08cbbc
marcquilloy <84653511+marcquilloy@users.noreply.github.com>
2022-11-30 11:23:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var password = "may52021";
+
+function passcheck() {
+
+if(document.getElementById('pass1').value != password) {
+alert('Wrong Password, Try Again.');
+return false;
+}
+
+if(document.getElementById('pass1').value != password) {
+    alert('Correct Password Makikita mo na ang Secret Message.');
+}
+}
